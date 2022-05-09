5f5c2a5427f3fb072045b4b910bd3611ca29a3d3
Swaraj Phadtare <1swarajphadtare@gmail.com>
2022-05-09 13:42:54 +0530

    React example with updated libraries

--- examples/react-app/src/App.tsx
+++ examples/react-app/src/App.tsx
@@ -1,52 +1,55 @@
-import React from "react";
-import "./App.css";
-
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import {
-  ConnectionProvider,
-  WalletProvider
-} from "@solana/wallet-adapter-react";
-import {
-  getLedgerWallet,
-  getPhantomWallet,
-  getSlopeWallet,
-  getSolflareWallet,
-  getSolletExtensionWallet,
-  getSolletWallet
-} from "@solana/wallet-adapter-wallets";
-import { clusterApiUrl } from "@solana/web3.js";
-import MyWallet from "./MyWallet";
-import SignIn from "./SignIn";
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter
+} from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import { FC, ReactNode, useMemo } from 'react';
+import MyWallet from './MyWallet';
+import SignIn from './SignIn';
 
-function App() {
-  // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
-  const network = WalletAdapterNetwork.Mainnet;
+export const App: FC = () => {
+    return (
+        <Context>
+            <Content />
+        </Context>
+    );
+};
 
-  // You can also provide a custom RPC endpoint
-  const endpoint = React.useMemo(() => clusterApiUrl(network), [network]);
+const Context: FC<{ children: ReactNode }> = ({ children }) => {
+    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+    const network = WalletAdapterNetwork.Mainnet;
 
-  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking --
-  // Only the wallets you configure here will be compiled into your application
-  const wallets = React.useMemo(
-    () => [
-      getPhantomWallet(),
-      getSlopeWallet(),
-      getSolflareWallet(),
-      getLedgerWallet(),
-      getSolletWallet({ network }),
-      getSolletExtensionWallet({ network }),
-    ],
-    [network]
-  );
+    // You can also provide a custom RPC endpoint.
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-  return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets}>
-      <div className="main">
+    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+    // Only the wallets you configure here will be compiled into your application, and only the dependencies
+    // of wallets that your users connect to will be loaded.
+    const wallets = useMemo(
+        () => [
+            new PhantomWalletAdapter(),
+            new GlowWalletAdapter(),
+            new SlopeWalletAdapter(),
+            new SolflareWalletAdapter({ network }),
+            new TorusWalletAdapter(),
+        ],
+        [network]
+    );
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+            <div className="main">
           <p className="sign">Sign in With Solana</p>
          <br/>
          <div>
-         <MyWallet />
+          <MyWallet />
           <SignIn/>
           <span id="postSignIn" style={{ display: "none" }}>
             <p>Public Key</p>
@@ -59,9 +62,12 @@ function App() {
           <img className='logo' src="https://app.tor.us/v1.22.2/img/web3auth.b98a3302.svg"/>
          </div>
       </div>
-      </WalletProvider>
-    </ConnectionProvider>
-  );
-}
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
 
-export default App;
+const Content: FC = () => {
+    return <WalletMultiButton />;
+};
+
