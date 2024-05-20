375411b8ceedc3a9b28a921c16369ef4c2dd9265
Tomas Rawski <rtomas@gmail.com>
2024-05-20 11:52:26 -0300

    fix polyfill issue

--- examples/vue-app/vue.config.js
+++ examples/vue-app/vue.config.js
@@ -4,17 +4,32 @@ const { ProvidePlugin } = require("webpack");
 module.exports = {
   transpileDependencies: true,
   devServer: {
-    port: 3000
+    port: 3000,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
   },
   configureWebpack: (config) => {
     config.resolve.fallback = {
       // crypto: require.resolve("crypto-browserify"),
-      stream: require.resolve("stream-browserify"),
-      assert: require.resolve("assert"),
-      crypto: require.resolve("crypto-browserify"),
+      //stream: require.resolve("stream-browserify"),
+      //assert: require.resolve("assert"),
+      //crypto: require.resolve("crypto-browserify"),
+      crypto: false,
+      stream: false,
+      assert: false,
+      os: false,
+      https: false,
+      http: false,
+      zlib: false,
     };
+    /* config.resolve.alias = {
+      ...config.resolve.alias,
+    }; */
     config.resolve.alias = {
       ...config.resolve.alias,
+      "bn.js": path.resolve(__dirname, "node_modules/bn.js"),
+      lodash: path.resolve(__dirname, "node_modules/lodash"),
     };
     config.plugins.push(new ProvidePlugin({ Buffer: ["buffer", "Buffer"] }));
     config.plugins.push(new ProvidePlugin({ process: ["process/browser"] }));
